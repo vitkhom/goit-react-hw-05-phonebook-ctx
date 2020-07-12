94ab9035b1d1bd385bb91b77d458e9162f0944fb
vitkhom <vitalii.s.khomenko@gmail.com>
2020-07-12 11:24:02 +0300

    Updates

--- precache-manifest.9c4a260ac399bf959907fede01cbbe3a.js
+++ precache-manifest.9c4a260ac399bf959907fede01cbbe3a.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d2f72f653f7d8e91a8b89f60607382e",
+    "revision": "eb09cfeeb7186336906bb8a5df81d5df",
     "url": "/goit-react-hw-05-phonebook-ctx/index.html"
   },
   {
-    "revision": "7dd0e447196bd2feaef3",
+    "revision": "71257703b3afad53bbcb",
     "url": "/goit-react-hw-05-phonebook-ctx/static/css/2.d34346ea.chunk.css"
   },
   {
-    "revision": "072711b2821b4087cc6b",
+    "revision": "7a0c0ecddb051420aebb",
     "url": "/goit-react-hw-05-phonebook-ctx/static/css/main.2e36fcc8.chunk.css"
   },
   {
-    "revision": "7dd0e447196bd2feaef3",
-    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.05d4eb8c.chunk.js"
+    "revision": "71257703b3afad53bbcb",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.31f6d1d9.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.05d4eb8c.chunk.js.LICENSE.txt"
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.31f6d1d9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "072711b2821b4087cc6b",
-    "url": "/goit-react-hw-05-phonebook-ctx/static/js/main.7c13b322.chunk.js"
+    "revision": "7a0c0ecddb051420aebb",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/main.eca609a0.chunk.js"
   },
   {
     "revision": "d281f072a4e1a9520414",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-05-phonebook-ctx/precache-manifest.ca1b251970077f5e80490b6baabc3dc6.js"
+  "/goit-react-hw-05-phonebook-ctx/precache-manifest.9c4a260ac399bf959907fede01cbbe3a.js"
 );
 
 self.addEventListener('message', (event) => {
